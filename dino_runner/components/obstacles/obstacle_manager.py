c22b4952075a4cbc8362330705b899820ed8acdc
Rubiana Perucci <rubi_perucci@hotmail.com>
2022-10-12 20:06:54 -0300

    Obstacles: Large Cactus, Bird (tarefa)

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
@@ -9,9 +10,14 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
-        if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+        obstacle_type = [
+            Cactus(),
+            Bird(),
+        ]
 
+        if len(self.obstacles) == 0:            
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
